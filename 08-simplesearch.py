b54bb5207b90c1e57a0c41ff2fd2ebf9e348709d
sammiewhimsy <samanthaanne.cabiad@gmail.com>
2025-11-05 17:03:15 +0400

    08-simplesearch.py

--- 08-simplesearch.py
+++ 08-simplesearch.py
@@ -1,9 +1,14 @@
 listofnames = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 huntthisguy = "Sam"
 fella = huntthisguy in listofnames
-print(f"{fella}, the name is a part of the list")
+print(f"{fella}, the name is {huntthisguy} a part of the list")
+# Created a list of names.
+# Created a variable for the specific name to be searched.
+# Created a second variable to determine if Sam is a part of the list.
+# Displayed the result, where "fella" is a boolean variable that confirms that Sam is in the list.
 
-# idk which one
+# I also created an alternate version using "if-else" with the "in" keyword as another way to check if Sam is in the list.
+# The "huntthisguy" variable is still there to specify the name to be searched.
 
 listofnames = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 huntthisguy = "Sam"
