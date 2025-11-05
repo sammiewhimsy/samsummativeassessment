160aeae1f987ef3a82620a8e5d084e2872027fb5
sammiewhimsy <samanthaanne.cabiad@gmail.com>
2025-11-05 16:59:47 +0400

    01-codingiscool.py

--- 01-codingiscool.py
+++ 01-codingiscool.py
@@ -1,5 +1,9 @@
-# coding is cool
+# 1 — Coding is cool
+
+# Making a variable for each word.
 word1 = "Coding"
 word2 = "is"
 word3 = "cool"
+# Then adding them together in parentheses by the print function.
+# The " " 's were added so there would be spaces in the output.
 print(word1 + " " + word2 + " " + word3)
