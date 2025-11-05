edb188fb66783aa142a82a47320d26e54bc546c8
sammiewhimsy <samanthaanne.cabiad@gmail.com>
2025-11-05 17:01:44 +0400

    03-biography.py

--- 03-biography.py
+++ 03-biography.py
@@ -1,14 +1,19 @@
-# biography
+# 3 — Biography
+
 bioinfo = {'Name': 'Sam Cabiad', 'Hometown': 'Philippines', 'Age': 18}
+# Stored information in key-value dictionary.
 for key, value in bioinfo.items():
     print(f"{key}: {value}")
+# Using items() method to return the list as tuples.
 
-# biography (the advanced  requirements one but i'm actually not good at this)
+# (The advanced  requirements one but i'm actually not good at this)
 namewoah = str(input("Enter name: "))
 hometownwoah = str(input("Enter hometown: "))
 agewoah = (input("Enter age: "))
+# Variables for user input.
 try:
     agewoaw = int(agewoah)
 except ValueError:
-    agewoaw = agewoah
-    print(f"Please enter the number itself next time!")
+    print("Please enter the number itself next time!")
+# Whenever anything that isn't a integer is entered, a ValueError is prompted.
+# To fix the issue, I used a ValueError exception to inform the user that the function received an invalid value.
