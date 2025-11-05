f7c570a8f32a6ca1b6dd6813e452ec46ffdeb105
sammiewhimsy <samanthaanne.cabiad@gmail.com>
2025-11-05 17:00:08 +0400

    02-simplesums.py

--- 02-simplesums.py
+++ 02-simplesums.py
@@ -1,5 +1,9 @@
-# simple sums
+# 2 — Simple sums
+
 integer1 = 8
 integer2 = 10
+# Two variables declared with set numbers.
 bakesthetwo = sum([integer1, integer2])
+# Third variable to store the sum of the first two variables.
+# The sum function will sum both integers when printed.
 print(bakesthetwo)
