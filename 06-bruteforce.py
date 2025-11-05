11823ca07292537fbad8d39b282188d2b540b294
sammiewhimsy <samanthaanne.cabiad@gmail.com>
2025-11-05 17:02:41 +0400

    06-bruteforce.py

--- 06-bruteforce.py
+++ 06-bruteforce.py
@@ -4,3 +4,7 @@ while dapass != "12345":
     dapass = (input("Enter password: "))
 
 print("Access granted!")
+
+# Variable for user input.
+# Using while loop to loop the prompt until the correct password is entered.
+# Using the Not Equal (!=) operator to stop the flow of the loop when the correct password is entered.
