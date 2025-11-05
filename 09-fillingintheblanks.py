b3ee4864a4db7eefd6508d38e17b8d9fadc4fdac
sammiewhimsy <samanthaanne.cabiad@gmail.com>
2025-11-05 17:03:52 +0400

    09-fillingintheblanks.py

--- 09-fillingintheblanks.py
+++ 09-fillingintheblanks.py
@@ -1,8 +1,10 @@
 def hello():
     print("Hello")
+# Typed "print("Hello")" so it'd print "Hello" if called.
 
 def main():
     hello()
+# The first function is called to display "Hello."
 
 if __name__ == '__main__':
     main()
