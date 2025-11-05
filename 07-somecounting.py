0ea13cdfd7903a17b8d7c28cbbe55ba5d887b5b7
sammiewhimsy <samanthaanne.cabiad@gmail.com>
2025-11-05 17:02:58 +0400

    07-somecounting.py

--- 07-somecounting.py
+++ 07-somecounting.py
@@ -1,29 +1,47 @@
 #0 to 50 in increments of 1
 onetofifty = 1
-for onetofifty in range(0,51,1):
+for onetofifty in range(1,51,1):
     print(onetofifty)
     onetofifty += 1
+# Variable set to 1.
+# For loop to loop a specific number of times, using range + parameters to set a starting, ending, and increment value.
+# First parameter set to 1 to start at 1. Second parameter set to 51 so it would end at 50. Third parameter to set increment value to 1.
+# "+=" operator to set the increment value by 1.
 
 #50 to 1 in decrements of 1
 fiftytoone = 50
-while fiftytoone > -1:
+while fiftytoone > 0:
     print(fiftytoone)
     fiftytoone -= 1
+# Variable set to 50 so it would be the starting value.
+# While loop to execute until the value is greater than 0 (it would stop at 1).
+# "-=" operator to set the decrement value by 1.
 
 #30 to 50 in increments of 1
 thirtytofifty = 1
 for thirtytofifty in range(30,51,1):
     print(thirtytofifty)
     thirtytofifty += 1
+# Variable set to 1.
+# For loop to loop a specific number of times, using range + parameters to set a starting, ending, and increment value.
+# First parameter set to 30 to start at 30. Second parameter set to 51 so it would end at 50. Third parameter to set increment value to 1.
+# "+=" operator to set the increment value by 1.
 
 #50 to 10 in decrements of 2
 fiftytoten = 50
 while fiftytoten > 8:
     print(fiftytoten)
     fiftytoten -= 2
+# Variable set to 50 so it would be the starting value.
+# While loop to execute until the value is greater than 8 (it would stop at 10 since the decrement value is 2).
+# "-=" operator to set the decrement value by 2.
 
 #100 to 200 in increments of 5
 hundredto2 = 5
 for hundredto2 in range(100,205,5):
     print(hundredto2)
     hundredto2 += 5
+# Variable set to 5.
+# For loop to loop a specific number of times, using range + parameters to set a starting, ending, and increment value.
+# First parameter set to 100 to start at 100. Second parameter set to 205 so it would end at 200. Third parameter to set increment value to 5.
+# "+=" operator to set the increment value by 5.
