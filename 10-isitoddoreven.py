4cfa4fac5c0d5bf13cfcd39765a6e6e17e4281fc
sammiewhimsy <samanthaanne.cabiad@gmail.com>
2025-11-05 17:04:10 +0400

    10-isitoddoreven.py

--- 10-isitoddoreven.py
+++ 10-isitoddoreven.py
@@ -5,17 +5,25 @@ def inputyaya():
             return x
         except ValueError:
             print("Invalid. Please enter an integer.")
+# First function is for the user input and to check if the value is valid.
+# When valid, the function will exit the program and return the value.
+# When invalid (ValueError), it will prompt an invalid message and loop until the user enters an integer.
 
 def evenorodd(numnum):
     if numnum % 2 == 0:
         return "Even"
     else:
         return "Odd"
+# Second function checks if the entered number is odd or even.
+# If the number is not divisible by 2, it will be odd.
     
 def main():
     numnum = inputyaya()
     boombayeah = evenorodd(numnum)
     print(f"The number {numnum} is an {boombayeah} number.")
+# "numnum" variable stores the user's input.
+# "boombayeah" is the result after checking if the number is odd or even
+# F-string used to directly embed both variables.
     
 if __name__ == '__main__':
     main()
